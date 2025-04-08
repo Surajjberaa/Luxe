44a916c4c3689cf92c3e45194303c4776816b7f0
Surajjberaa <berasuraj08@gmail.com>
2025-04-08 19:03:49 +0530

    minor changes

--- client/src/components/aceternity/Cards.jsx
+++ client/src/components/aceternity/Cards.jsx
@@ -14,7 +14,7 @@ export function Cards({imgUrl, gifUrl, title, description, link, props}) {
       
       <div
         className={cn(
-          `group w-full  cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end border border-transparent dark:border-neutral-800`
+          `group mt-10 cursor-pointer overflow-hidden relative card h-[500px] w-[320px] rounded-md shadow-xl mx-auto flex flex-col justify-end border border-transparent dark:border-neutral-800`
   )}  
         
       >
@@ -27,16 +27,16 @@ export function Cards({imgUrl, gifUrl, title, description, link, props}) {
         className="absolute h-full w-full object-cover group-hover:opacity-100 opacity-0 transition-all duration-500 ease-in-out"
         // style={{ display: "none" }}
       />
-        <div className="text relative z-50 p-2">
-          <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative">
+        <div className="text relative z-50 p-2 h-full w-full flex flex-col justify-end ">
+          <h1 className="font-bold text-xl md:text-3xl text-gray-50 absolute top-0 py-4 ">
             {title}
           </h1>
-          <p className="font-normal text-base text-gray-50 relative my-4">
+          <p className="font-normal text-base text-gray-50 relative my-4 text-center ">
             {description}
           </p>
           <div className="flex justify-center items-center">
 
-          <button className="rounded-full h-10 w-30 bg-white backdrop-blur-2xl opacity-50 text-center text-black" onClick={() => navigate(link)}>View 3d</button>
+          <button className="rounded-full h-10 w-30 bg-gradient-to-r from-black to-gray-900 text-white text-center cursor-pointer border-2 border-gold-500 transition duration-300 ease-in-out transform hover:scale-110 hover:opacity-90 shadow-lg hover:shadow-2xl " onClick={() => navigate(link)}>View 3D</button>
           </div>
         </div>
       </div>
