b118ef205dbbd6e5ae45ea3c2ce5a1462900dba5
Surajjberaa <berasuraj08@gmail.com>
2025-04-10 04:44:23 +0530

    Changed default theme to dark

--- client/src/contexts/ThemeContext.jsx
+++ client/src/contexts/ThemeContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('dark');
 
   useEffect(() => {
     const localTheme = localStorage.getItem('theme');
@@ -12,7 +12,7 @@ export const ThemeProvider = ({ children }) => {
       document.documentElement.classList.add(localTheme);
     } else {
       document.documentElement.classList.add('light');
-      localStorage.setItem('theme', 'light');
+      localStorage.setItem('theme', 'dark');
     }
   }, []);
 
